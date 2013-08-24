db74a6c8eb8b52d97ad26820fc7c084784a7ea6a
Matt Beedle <mattbeedle@googlemail.com>
2013-08-24 19:46:44 +0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Salesflip::Application.config.secret_key_base = '83f00a553fd8d2bc5190c8bc0e6b0f0fb66a18a237e8d907f76c0dd3a5aa5158cf327671925e722bf046ed9cd3af06790e8e492e69a35335abc1f538b463c753'
